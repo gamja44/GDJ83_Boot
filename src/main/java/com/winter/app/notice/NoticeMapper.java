2f35ce6c3bd8722a6864e14b030301d05c4bbd25
gamja44 <yilin257@naver.com>
2024-09-03 09:32:13 +0900

    commit

--- src/main/java/com/winter/app/notice/NoticeMapper.java
+++ src/main/java/com/winter/app/notice/NoticeMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface NoticeMapper {
 	
 	//전체리스트보는 메서드
-	public List<NoticeVO> getList() throws Exception;
+	public List<NoticeVO> getList(Long num) throws Exception;
 	
 	//insert메서드
 	//매퍼 어노테이션을 사용해서 sqlSession을 사용하지않아도된다
