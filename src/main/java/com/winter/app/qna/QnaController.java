8d6921b65ec63a4e79c2e38139af64c5c2af7edd
gamja44 <yilin257@naver.com>
2024-09-04 12:36:00 +0900

    commit

--- src/main/java/com/winter/app/qna/QnaController.java
+++ src/main/java/com/winter/app/qna/QnaController.java
@@ -3,11 +3,14 @@ package com.winter.app.qna;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.winter.app.util.Pager;
 
@@ -22,7 +25,15 @@ public class QnaController {
 	
 	@Autowired
 	private QnaService qnaService;
+	@Value("${board.qna}")
+	private String board;
 	
+	
+	@ModelAttribute("board")
+	public String getBoard() {
+		return this.board;
+	}
+		
 	@GetMapping("list")
 	public void getList(Pager pager, Model model)throws Exception{
 		//Pager pager = new Pager(); 매개변수의 의미
@@ -38,8 +49,8 @@ public class QnaController {
 	public void add()throws Exception{}
 	
 	@PostMapping("add")
-	public String add(QnaVO qnaVO) throws Exception{
-		int result = qnaService.add(qnaVO);
+	public String add(QnaVO qnaVO, MultipartFile[] attaches) throws Exception{
+		int result = qnaService.add(qnaVO, attaches);
 		return "redirect:./list";
 	}
 	@GetMapping("detail")
--- src/main/java/com/winter/app/qna/QnaService.java
+++ src/main/java/com/winter/app/qna/QnaService.java
@@ -3,10 +3,15 @@ package com.winter.app.qna;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.winter.app.util.FileManager;
 import com.winter.app.util.Pager;
 
+import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -16,19 +21,36 @@ public class QnaService {
 	@Autowired
 	private QnaMapper qnaMapper;
 	
+	@Value("${app.upload}")
+	private String upload;
+	@Value("${board.qna}")
+	private String name;
+	
+	@Autowired
+	private FileManager fileManager;
+		
 	public List<QnaVO> getList(Pager pager)throws Exception{
 		pager.makeRow();
-		
+		log.info("Upload Path : {}", upload);
 		return qnaMapper.getList(pager);
 	}
 	
-	public int add(QnaVO qnaVO)throws Exception{
+	public int add(QnaVO qnaVO, MultipartFile [] attaches)throws Exception{
 		log.info("================insert before boardNum: {} ", qnaVO.getBoardNum());
-		int result=qnaMapper.add(qnaVO);
+		//int result=qnaMapper.add(qnaVO);
 		log.info("================insert after boardNum: {} ", qnaVO.getBoardNum());
-		result = qnaMapper.refUpdate(qnaVO);
+		//result = qnaMapper.refUpdate(qnaVO);
 		
-		return result;
+		//파일을 HDD에 저장 후 DB에 정보를 추가
+		for(MultipartFile mf : attaches) {
+			if(mf==null || mf.isEmpty()) {
+				continue;
+			}
+			 String fileName= fileManager.fileSave(upload+name, mf); //D:/upload/qna
+				log.info("저장된 파일명 : {}",fileName);
+		}
+		
+		return 0;
 	}
 	
 	public QnaVO getDetail(QnaVO qnaVO) throws Exception{
