48f770ae2771e5b24b4d69f3854b4e8ea04707b8
gamja44 <yilin257@naver.com>
2024-09-05 11:47:23 +0900

    commit

--- src/main/java/com/winter/app/qna/QnaService.java
+++ src/main/java/com/winter/app/qna/QnaService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@Transactional(rollbackFor = Exception.class)
 public class QnaService {
 	
 	@Autowired
@@ -36,11 +38,19 @@ public class QnaService {
 		return qnaMapper.getList(pager);
 	}
 	
+	//문제발생시 롤백을 해주겠다 
+	//메서드마다 transactional을 주던가
+	//클래스에 선언하는 방식도 있다
+    //	@Transactional(rollbackFor = Exception.class)
 	public int add(QnaVO qnaVO, MultipartFile [] attaches)throws Exception{
 		//log.info("================insert before boardNum: {} ", qnaVO.getBoardNum());
 		int result=qnaMapper.add(qnaVO);
 		//log.info("================insert after boardNum: {} ", qnaVO.getBoardNum());
 		result = qnaMapper.refUpdate(qnaVO);
+		
+		if(result==0) {
+			throw new Exception();
+		}
 		
 		//파일을 HDD에 저장 후 DB에 정보를 추가
 		for(MultipartFile mf : attaches) {
--- src/test/java/com/winter/app/qna/QnaMapperTest.java
+++ src/test/java/com/winter/app/qna/QnaMapperTest.java
@@ -7,16 +7,20 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.winter.app.util.Pager;
 
 @SpringBootTest
+@Transactional //모든 테스트 메서드 실행 후 전부 rollback처리, 예외가 없어도 롤백시키겠다
 class QnaMapperTest {
 	
 	@Autowired
 	private QnaMapper qnaMapper;
 	
 	@Test
+	@Rollback(false) //메서드 실행 후 rollback하지 않음
 	void getDetailTest() throws Exception{
 		QnaVO qnaVO = new QnaVO();
 		qnaVO.setBoardNum(110L);
@@ -26,24 +30,23 @@ class QnaMapperTest {
 	}
 	
 	//test개발주도방식을 사용해보기위한 메서드
-//	@Test
-//	void addTest() throws Exception{
-//		for(int i=4;i<110;i++) {
-//		
-//			QnaVO qnaVO = new QnaVO();
-//			qnaVO.setBoardContents("c3"+i);
-//			qnaVO.setBoardTitle("t3"+i);
-//			qnaVO.setBoardWriter("w3"+i);
-//			qnaVO.setRef((long)i);
-//			qnaVO.setStep(0L);
-//			qnaVO.setDepth(0L);
-//			int result = qnaMapper.add(qnaVO);	
-//			if(i%10==0) {
-//				Thread.sleep(500);
-//			}
-//		
-//		}
-//	}
+	@Test
+	void addTest() throws Exception{
+		for(int i=4;i<110;i++) {
+		
+			QnaVO qnaVO = new QnaVO();
+			qnaVO.setBoardContents("c3"+i);
+			qnaVO.setBoardTitle("t3"+i);
+			qnaVO.setBoardWriter("w3"+i);
+			qnaVO.setRef((long)i);
+			qnaVO.setStep(0L);
+			qnaVO.setDepth(0L);
+			int result = qnaMapper.add(qnaVO);	
+			if(i%10==0) {
+				Thread.sleep(500);
+			}	
+		}
+	}
 	
 	@Test
 	void getListTest()throws Exception {
